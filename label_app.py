1a89fcc2ee7aee75f0fe23943e1176996b195217
mcorvasce <michael.corvasce@gmail.com>
2025-08-07 00:30:33 -0400

    Update label_app.py

--- label_app.py
+++ label_app.py
@@ -73,8 +73,9 @@ bin_weight = st.number_input("Bin Gross Weight (Peso Bruto del Bin, lbs)", min_v
 
 # Generate Label
 if st.button("Generate Label / Generar Etiqueta") and valid_selection:
-    eastern = pytz.zone("US/Eastern")
-    timestamp = datetime.datetime.now(pytz.timezone("US/Eastern")).strftime("%Y-%m-%d %H:%M:%S %Z")
+    eastern = pytz.timezone("US/Eastern")
+
+    timestamp = datetime.datetime.now(eastern).strftime("%Y-%m-%d %H:%M:%S")
 
     width, height = landscape((6 * inch, 4 * inch))
     c = canvas.Canvas(PDF_FILE, pagesize=(width, height))
